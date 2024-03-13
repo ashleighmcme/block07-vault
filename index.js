55f5cf409790a82c449910c5f31ff820d81a561a
Ashleigh McMahon <ashleighmc@me.com>
2024-03-12 20:23:33 -0600

    Updating and linking html and js file

---
+++ index.js
@@ -0,0 +1,18 @@
+//write the pseudo code for steps on solving the exercise
+/* Create a string saying "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+Assign three variables using uniquw arithmetic operator 
+Comment your code
+Create a pop up dialog box displaying the vault code
+upload to Git
+Combination is 10-40-39
+*/
+let chosenOne = 'You have received this message because you have been chosen to open an important vault.  Here is the secret combination:';
+let firstNum=(100/10);
+let secondNum=(2*20);
+let thirdNum=(30+9);
+let unlocked=(`${firstNum}` + ` ` + `${secondNum}` + ` ` + `${thirdNum}`)
+console.log (unlocked);
+function alert(unlocked);
+
+
+
