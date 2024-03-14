bfdbfec008c5751db97b253841d991322db195d8
Ashleigh McMahon <ashleighmc@me.com>
2024-03-13 20:29:35 -0600

    Trying to upload a different way

--- index.js
+++ index.js
@@ -6,22 +6,36 @@ Create a pop up dialog box displaying the vault code
 upload to Git
 Combination is 10-40-39
 */
+/* adding my constant for Chosen One*/
 const chosenOne = "You have received this message because you have been chosen to open an important vault.  Here is the secret combination:";
-let firstNum=(100/10);
-let secondNum=(2*20);
-let thirdNum=(30+9);
+/* I used let for my variable because in my head I'm thinking that when I write a program, let will show me areas where I know variables can change.*/
+let firstNum=(100/10); //first number using an arithmetic operator
+let secondNum=(2*20); //second number using an arithmetic operator
+let thirdNum=(30+9); //third number using an arithmetic operator
 
 //function unlocked=(`${firstNum}` + ` ` + `${secondNum}` + ` ` + `${thirdNum}`)
 //console.log (unlocked);
 // alert(
 //    console.log (unlocked)
 // );
+//above was code where I was trying to use a function to get my alert box but I know I am missing pieces
+
 console.log(chosenOne);
 console.log(firstNum);
 console.log(secondNum);
 console.log(thirdNum);
+//not sure if I need the console logs other than maybe it would be helpful during debugging
+
 
 document.getElementById('chosenOne').innerHTML = chosenOne;
+const congrats = "You have successfully unlocked the vault.";
+function showAlert(congrats){
+    var modal = document.getElementById("customAlert");
+  var alertMessage = document.getElementById("alertMessage");
+  alertMessage.innerHTML = message;
+  modal.style.display = "block";
+}
 setTimeout(function() {alert(firstNum + "-" + secondNum + "-" + thirdNum);},1500);
+//set timeout delays popup a few moments in order to read landing page.
 
 
