bdc53e26d16bffc22c11d4b7b77e50b3301b5119
Ashleigh McMahon <ashleighmc@me.com>
2024-03-13 18:50:01 -0600

    Changes

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ Create a pop up dialog box displaying the vault code
 upload to Git
 Combination is 10-40-39
 */
-let chosenOne = 'You have received this message because you have been chosen to open an important vault.  Here is the secret combination:';
+const chosenOne = "You have received this message because you have been chosen to open an important vault.  Here is the secret combination:";
 let firstNum=(100/10);
 let secondNum=(2*20);
 let thirdNum=(30+9);
-let unlocked=(`${firstNum}` + ` ` + `${secondNum}` + ` ` + `${thirdNum}`)
-console.log (unlocked);
-function alert(unlocked);
-<script>
-        function chosenOne() {
-          alert("ChosenOne");
-        }
-        </script>
+
+//function unlocked=(`${firstNum}` + ` ` + `${secondNum}` + ` ` + `${thirdNum}`)
+//console.log (unlocked);
+// alert(
+//    console.log (unlocked)
+// );
+console.log(chosenOne);
+console.log(firstNum);
+console.log(secondNum);
+console.log(thirdNum);
+
+document.getElementById('chosenOne').innerHTML = chosenOne;
+setTimeout(function() {alert(firstNum + "-" + secondNum + "-" + thirdNum);},1500);
+
+
